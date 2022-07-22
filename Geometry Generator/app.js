69192bdf396838bb80c1d46878beaf3e4df3ead3
Jakariya06 <56745344+Jakariya06@users.noreply.github.com>
2022-07-22 07:57:28 +0700

    Add files via upload

---
+++ Geometry Generator/app.js
@@ -0,0 +1,36 @@
+const btn = document.getElementById("btn")
+const shapes = [
+    "quad-circle-1",
+    "quad-circle-2",
+    "quad-circle-3",
+    "quad-circle-4",
+    "triangle-1",
+    "triangle-2",
+    "triangle-3",
+    "triangle-4",
+    "circle",
+]
+
+const colors = [
+    "#A6FE49",
+    "#F5FB35",
+    "#17D9F8",
+    "#FA1010",
+    "#F1A205",
+    "#E9EFFA"
+]
+
+const boxes = document.querySelectorAll('.container div')
+
+let generatePattern = () => {
+    boxes.forEach((box) => {
+        box.className = "";
+        let i = Math.floor(Math.random()* shapes.length);
+        let j = Math.floor(Math.random()* boxes.length);
+        box.classList.add(shapes[i])
+        box.style.backgroundColor = colors[j]
+    })
+}
+
+btn.addEventListener("click", generatePattern)
+window.addEventListener("load", generatePattern)
